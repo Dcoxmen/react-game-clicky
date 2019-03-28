4e94461e475499d02d61a45d9d04a25146bddbc8
Dave Cox <davedvst@gmail.com>
2019-03-27 19:28:26 -0700

    Updates

---
+++ precache-manifest.05b78e02955c4fa80cc4a464b02b5325.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "94dcff864af04f005ba5580622bdbbd4",
+    "url": "/react-game-clicky/static/media/eyes5.94dcff86.png"
+  },
+  {
+    "revision": "69438a99ffa2a029513f",
+    "url": "/react-game-clicky/static/css/main.4d50fa08.chunk.css"
+  },
+  {
+    "revision": "79023b429b3ec0284992",
+    "url": "/react-game-clicky/static/js/runtime~main.82851adc.js"
+  },
+  {
+    "revision": "7acbbda12e8e3ef759a2",
+    "url": "/react-game-clicky/static/js/2.71f1e44a.chunk.js"
+  },
+  {
+    "revision": "fd6a33c36d19e6da8972e7c7d10ba4ec",
+    "url": "/react-game-clicky/static/media/eyes1.fd6a33c3.png"
+  },
+  {
+    "revision": "20b5ec399f55065f723cefafb6c360d8",
+    "url": "/react-game-clicky/static/media/eyes2.20b5ec39.png"
+  },
+  {
+    "revision": "a1bdaf35e11b22ae4d77b028bc9dfe81",
+    "url": "/react-game-clicky/static/media/eyes3.a1bdaf35.png"
+  },
+  {
+    "revision": "ed73b6582c1c4737e616f9c1b771c79f",
+    "url": "/react-game-clicky/static/media/eyes4.ed73b658.png"
+  },
+  {
+    "revision": "69438a99ffa2a029513f",
+    "url": "/react-game-clicky/static/js/main.09dddbb6.chunk.js"
+  },
+  {
+    "revision": "f97ed937cffd0ada0635fc737b1d1255",
+    "url": "/react-game-clicky/static/media/eyes6.f97ed937.png"
+  },
+  {
+    "revision": "4a54a1c6a507890503a48d201fcc72ca",
+    "url": "/react-game-clicky/static/media/eyes7.4a54a1c6.png"
+  },
+  {
+    "revision": "24b55d732dd861a2da076fe552ff75c9",
+    "url": "/react-game-clicky/static/media/eyes8.24b55d73.png"
+  },
+  {
+    "revision": "f52d091f4b127e76039ef6e25f723eab",
+    "url": "/react-game-clicky/static/media/eyes9.f52d091f.png"
+  },
+  {
+    "revision": "5bb565e5a7b1d734ce1049ef271d5443",
+    "url": "/react-game-clicky/static/media/eyes10.5bb565e5.png"
+  },
+  {
+    "revision": "c3184a7cc3645e0b198717271d6afd3b",
+    "url": "/react-game-clicky/static/media/eyes11.c3184a7c.png"
+  },
+  {
+    "revision": "6ee41fb00ad6e86b15c79b6afbaab66e",
+    "url": "/react-game-clicky/static/media/eyes12.6ee41fb0.png"
+  },
+  {
+    "revision": "59367e6165b2ff692892387ca21c4ef3",
+    "url": "/react-game-clicky/index.html"
+  }
+];
